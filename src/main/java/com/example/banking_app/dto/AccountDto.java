d5e33fdb1d1e69989bc18d299b867f3c80da8f84
Smim7 <somayamorshed7@gmail.com>
2025-07-17 00:13:09 -0400

    create class AccountMapper

--- src/main/java/com/example/banking_app/dto/AccountDto.java
+++ src/main/java/com/example/banking_app/dto/AccountDto.java
@@ -1,8 +1,10 @@
 package com.example.banking_app.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class AccountDto {
     private Long id;
     private String accountHolderName;
